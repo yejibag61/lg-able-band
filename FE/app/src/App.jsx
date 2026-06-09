6b134cb850d4d11084db6fa1e853c1311c9e9771
yejibag61 <yejibag61@gmail.com>
2026-06-09 15:42:01 +0900

    샘플 앱 초기화

--- FE/app/src/App.jsx
+++ FE/app/src/App.jsx
@@ -1,15 +1,7 @@
 import './App.css'
 
 function App() {
-  return (
-    <main className="page">
-      <header className="header">
-        <p className="kicker">App Frontend</p>
-        <h1>LG Able Band</h1>
-      </header>
-      <p className="notice">App dashboard is ready.</p>
-    </main>
-  )
+  return <main className="app-root" />
 }
 
 export default App
--- FE/wearable/src/App.jsx
+++ FE/wearable/src/App.jsx
@@ -1,15 +1,7 @@
 import './App.css'
 
 function App() {
-  return (
-    <main className="page">
-      <header className="header">
-        <p className="kicker">Wearable Frontend</p>
-        <h1>LG Able Band</h1>
-      </header>
-      <p className="notice">Wearable dashboard is ready.</p>
-    </main>
-  )
+  return <main className="app-root" />
 }
 
 export default App
